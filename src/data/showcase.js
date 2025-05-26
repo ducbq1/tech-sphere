787cbd1ee2c01e4ce5ea1864296d3945cb52adc9
ducbq1@viettel.com.vn <ducbq1@viettel.com.vn>
2025-05-26 11:29:34 +0700

    UPDATE

---
+++ src/data/showcase.js
@@ -0,0 +1,15 @@
+// src/data/showcase.js
+export const users = [
+  {
+    caption: "My Awesome Project",
+    image: "/img/logo.png",
+    infoLink: "https://myproject.example.com",
+    pinned: true,
+  },
+  {
+    caption: "Another Project",
+    image: "/img/another-logo.png",
+    infoLink: "https://another.example.com",
+    pinned: false,
+  },
+];
